4b219af7a326a6a2ed9e3427e6357c24ea0a8455
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2023-02-25 22:31:29 +0000

    Parse logs and print bids

--- bid.scala
+++ bid.scala
@@ -1,3 +1,4 @@
+import scala.io.Source
 
 object Main {
   type Card = Int
@@ -10,7 +11,7 @@ object Main {
   val c8  = 0x02
   val c7  = 0x01
 
-  type Color = Int
+  type Suite = Int
   val notrump  = 40
   val alltrump = 32
   val spade    = 24
@@ -22,19 +23,6 @@ object Main {
     def openInFour: Boolean = ???
     def jn3rd: Boolean = ???
     def suites: Int = ???
-
-    def show(color: Color): String = {
-      val sb = new StringBuilder()
-      if (contains(cA << color)) sb.append("A ")
-      if (contains(c10 << color)) sb.append("10 ")
-      if (contains(cK << color)) sb.append("K ")
-      if (contains(cQ << color)) sb.append("Q ")
-      if (contains(cJ << color)) sb.append("J ")
-      if (contains(c9 << color)) sb.append("9 ")
-      if (contains(c8 << color)) sb.append("8 ")
-      if (contains(c7 << color)) sb.append("7 ")
-      sb.toString
-    }
 
     def aces: Int =
       (if (contains(cA << spade)) 1 else 0) +
@@ -52,6 +40,12 @@ object Main {
       (value & card) != 0
   }
 
+  sealed trait RawBid
+  case class ValueBid(n: Int, c: Char) extends RawBid
+  case object Passs extends RawBid
+  case object Doubles extends RawBid
+  case object Redoubles extends RawBid
+
   object Bid extends Enumeration {
     type Bid = Value
     val P10Rz, P10Sw, P10SA, P10TA,
@@ -68,34 +62,32 @@ object Main {
   case class System(bids: (Bid, Hand => Boolean, System)*)
 
   // As player 1
-  def biddingSystem = System(
-     (Pass, _.aces < 2, ???),
-     (P10Sw, _.aces >= 2, ???),
-     (P10S A, _.aces >= 3, ???),
-     (P10TA, x => x.aces >= 1 && x.jacks >= 2, ???),
-     (P20Sw, _ => true, ???),
-     (P20SA, _.aces == 2, ???),
-     (P20TA, _.jacks >= 3, ???),
-     (P30Sw, x => x.suites == 4 && x.jn3rd, ???),
-     (P30SA, _.aces == 4, ???),
-     (P30TA, _.jacks == 4, ???),
-     (P40Sw, x => x.suites == 3 && x.jn3rd, ???),
-     (P40SA, ???, ???),
-     (P40TA, ???, ???),
-     (P50Sw, x => x.jn3rd /*&& x.contains(Sw+1)*/, ???),
-     (P50SA, ???, ???),
-     (P50TA, ???, ???),
-     (P60Sw, x => x.jn3rd /*&& x.contains(Sw+2)*/, ???),
-     (P60SA, ???, ???),
-     (P60TA, ???, ???),
-     (P70Sw, x => x.jn3rd /*&& x.contains(Sw+3)*/, ???),
-     (P70SA, ???, ???),
-     (P70TA, ???, ???)
+  val system = System(
+     (Pass, _.aces < 2, null),
+     (P10Sw, _.aces >= 2, null),
+     (P10SA, _.aces >= 3, null),
+     (P10TA, x => x.aces >= 1 && x.jacks >= 2, null),
+     (P20Sw, _ => true, null),
+     (P20SA, _.aces == 2, null),
+     (P20TA, _.jacks >= 3, null),
+     (P30Sw, x => x.suites == 4 && x.jn3rd, null),
+     (P30SA, _.aces == 4, null),
+     (P30TA, _.jacks == 4, null),
+     (P40Sw, x => x.suites == 3 && x.jn3rd, null),
+     (P40SA, null, null),
+     (P40TA, null, null),
+     (P50Sw, x => x.jn3rd /*&& x.contains(Sw+1)*/, null),
+     (P50SA, null, null),
+     (P50TA, null, null),
+     (P60Sw, x => x.jn3rd /*&& x.contains(Sw+2)*/, null),
+     (P60SA, null, null),
+     (P60TA, null, null),
+     (P70Sw, x => x.jn3rd /*&& x.contains(Sw+3)*/, null),
+     (P70SA, null, null),
+     (P70TA, null, null)
   )
 
-  def check(h1: Hand, h2: Hand, starting: Boolean, bids: List[Bid]): Boolean = {
-    assert(starting)
-
+  def check(h1: Hand, h2: Hand, bids: List[Bid]): Boolean = {
     def check0(h1: Hand, h2: Hand, bids: List[Bid], system: System): Boolean = {
       bids match {
         case Pass :: Pass :: Pass :: Pass =>
@@ -104,59 +96,155 @@ object Main {
           val (bid, pred, subsystem) = system.bids.find(_._1 == b).get
           pred(h1)
           // check0(h2, h1, bs, subsystem)
+        case _ =>
+          false
       }
     }
 
-    check0(h1, h2, bids, biddingSystem)
+    check0(h1, h2, bids, system)
   }
 
   def main(args: Array[String]): Unit = {
-    val h1 = Hand(
-      cA << spade | cA << diamond | c10 << heart | cJ << club |
-      c8 << club | c9 << spade | cK << heart | c8 << diamond)
-
-    val h2 = Hand(
-     c7 << spade | cJ << diamond | cA << heart | cA << club |
-     c10 << club | c10 << spade | c9 << club | cJ << spade)
-
-    val bids = List(
-      P10Sw, Pass, P10Rz, P10Sw,
-      P10Rz, Pass, P30SA, Pass,
-      Pass, Pass, Pass
-    )
-
-    // assert(check(h1, h2, true, bids))
-    println(show(h1, h1, h2, Hand(c7 << spade), bids))
+    args.foreach { path =>
+      val round = Round.read(path)
+      if (!check(round.h1, round.h3, normalize(round.bids)))
+        println(round)
+    }
   }
 
-  def show(x: Hand, name: String): List[String] = {
-    val s = x.show(spade)
-    val d = x.show(diamond)
-    val h = x.show(heart)
-    val c = x.show(club)
-    val m = s.size max d.size max h.size max c.size
-    "╔══" + "".padTo(m, "═").mkString + "═╗   " ::
-    "║ ♠ " + s.padTo(m, " ").mkString + "║ " + name ::
-    "║ ♥ " + d.padTo(m, " ").mkString + "║   " ::
-    "║ ♣ " + h.padTo(m, " ").mkString + "║   " ::
-    "║ ♦ " + c.padTo(m, " ").mkString + "║   " ::
-    "╚══" + "".padTo(m, "═").mkString + "═╝   " :: Nil
+  def normalize(bids: List[RawBid]): List[Bid] = {
+    return Nil
   }
 
-  def show(h1: Hand, h2: Hand, h3: Hand, h4: Hand, bids: List[Bid]): String = {
-    val s1 = show(h1, "P1")
-    val s2 = show(h2, "P2")
-    val s3 = show(h3, "P3")
-    val s4 = show(h4, "P4")
-    (
-      s3.map(" " * s2.head.length + _) ++
-      s2.map(_ + " " * s1.head.length.max(s3.head.length))
-        .zip(s4)
-        .map { case (x, y) => x + y } ++
-      s1.map(" " * s2.head.length + _)
-    ).zipAll(
-      bids.scan
-      , "", ""
-    ).mkString("\n")
+  case class Round(h1: Hand, h2: Hand, h3: Hand, h4: Hand, bids: List[RawBid]) {
+    override def toString: String = {
+      def show(x: Hand): List[String] = {
+        def shov(suite: Suite): String = {
+          val sb = new StringBuilder()
+          if (x.contains(cA << suite)) sb.append("A ")
+          if (x.contains(c10 << suite)) sb.append("10 ")
+          if (x.contains(cK << suite)) sb.append("K ")
+          if (x.contains(cQ << suite)) sb.append("Q ")
+          if (x.contains(cJ << suite)) sb.append("J ")
+          if (x.contains(c9 << suite)) sb.append("9 ")
+          if (x.contains(c8 << suite)) sb.append("8 ")
+          if (x.contains(c7 << suite)) sb.append("7 ")
+          sb.toString
+        }
+        val s = shov(spade)
+        val d = shov(diamond)
+        val h = shov(heart)
+        val c = shov(club)
+        val m = s.size max d.size max h.size max c.size
+        "╔══" + "".padTo(m, "═").mkString + "═╗" ::
+        "║ ♠ " + s.padTo(m, " ").mkString + "║" ::
+        "║ ♥ " + h.padTo(m, " ").mkString + "║" ::
+        "║ ♣ " + c.padTo(m, " ").mkString + "║" ::
+        "║ ♦ " + d.padTo(m, " ").mkString + "║" ::
+        "╚══" + "".padTo(m, "═").mkString + "═╝" :: Nil
+      }
+      def p(i: Int, lines: List[String]): List[String] =
+        lines.zipWithIndex.map {
+          case (l, 1) => l + " P" + i
+          case (l, _) => l + "   "
+        }.toList
+      val s1 = p(1, show(h1))
+      val s2 = p(2, show(h2))
+      val s3 = p(3, show(h3))
+      val s4 = p(4, show(h4))
+
+      var bidLog = "╔════════════════╗" :: "Bidding rounds:" :: Nil
+      bids.zipWithIndex.map {
+        case (x, i) => (x, "P" + (i % 4 + 1))
+      }.map {
+        case (Passs, p) => s"║ $p passes"
+        case (Doubles, p) => s"║ $p doubles"
+        case (Redoubles, p) => s"║ $p redoubles"
+        case (ValueBid(n, c), p) =>
+          val s = c match {
+            case '1' => "♠" case '2' => "♥" case '3' => "♣"
+            case '4' => "♦" case '5' => "TA" case '6' => "SA"
+          }
+          s"║ $p bids $n $s"
+      }.foreach(s => bidLog ::= s.padTo(17, " ").mkString + "║")
+      bidLog ::= "╚════════════════╝"
+      bidLog = bidLog.reverse
+
+      val board =
+        s3.map(" " * s2.head.length + _) ++
+        s2.map(_ + " " * s1.head.length.max(s3.head.length))
+          .zip(s4)
+          .map { case (x, y) => x + y } ++
+        s1.map(" " * s2.head.length + _)
+
+      board
+        .map(_.padTo(board.maxBy(_.size).size, " ").mkString)
+        .zipAll(bidLog, "", "")
+        .map { case (x, y) => x + "  " + y }
+        .mkString("\n")
+    }
   }
+
+  object Round {
+    def read(path: String): Round = {
+      var h1, h2, h3, h4 = 0
+      var xs = List.empty[RawBid]
+
+      val pass = raw"P\d passes".r
+      val bids = raw"P\d bids (\d+) (\d) \([a-z ]+\)".r
+      val p1plays = raw"\dP1 plays (..?) (spade|diamond|heart|club)".r
+      val p2plays = raw"\dP2 plays (..?) (spade|diamond|heart|club)".r
+      val p3plays = raw"\dP3 plays (..?) (spade|diamond|heart|club)".r
+      val p4plays = raw"\dP4 plays (..?) (spade|diamond|heart|club)".r
+      val doubles = raw"P\d doubles!".r
+      val redoubl = raw"P\d redoubles!".r
+      val skip1 = raw"Everybody passes, \d+ \d for P\d".r
+      val skip2 = raw"\dP\d wins the trick".r
+      val skip3 = raw"P\d wins the last trick \(.10 pts\)".r
+      val skip4 = raw"Bid successful, \d+ to \d+!".r
+      val skip5 = raw"P\d scores \d+ points".r
+      val skip6 = raw"P\d is now the first player".r
+      val skip7 = raw"The end of the game: P\d and P\d win!".r
+      val skip8 = raw"The end of the game: P\d wins!".r
+      val skip9 = raw"P\d scores no point".r
+      val skipA = raw"P\d says Belote!".r
+      val skipB = raw"P\d says Rebelote!".r
+      val skipC = raw"Bid fails, \d+ to \d+!".r
+      val skipD = raw"Waiting for redouble".r
+      val skipE = raw"P\d does not redouble.".r
+      val wat = raw"The end of the game: P1, P2 and P4 win!".r
+
+      def parseCard(c: String): Card = c match {
+        case "A" => cA case "10" => c10 case "K" => cK case "Q" => cQ
+        case "J" => cJ case "9"  => c9  case "8" => c8 case "7" => c7
+        case _ => println("Unknown card? " + path); cA
+      }
+
+      def parseSuite(s: String): Suite = s match {
+        case "spade" => spade case "diamond" => diamond
+        case "heart" => heart case "club" => club
+        case _ => println("Unknown suite? " + path); club
+      }
+
+      Source.fromFile(path).getLines.foreach {
+        case pass() => xs ::= Passs
+        case bids(n, c) => xs ::= ValueBid(n.toInt, c.head)
+        case p1plays(c, s) => h1 |= parseCard(c) << parseSuite(s)
+        case p2plays(c, s) => h2 |= parseCard(c) << parseSuite(s)
+        case p3plays(c, s) => h3 |= parseCard(c) << parseSuite(s)
+        case p4plays(c, s) => h4 |= parseCard(c) << parseSuite(s)
+        case doubles() => xs ::= Doubles
+        case redoubl() => xs ::= Redoubles
+        case skip1() => case skip2() => case skip3() => case skip4() =>
+        case skip5() => case skip6() => case skip7() => case skip8() =>
+        case skip9() => case skipA() => case skipB() => case skipC() =>
+        case skipD() => case skipE() =>
+        case wat() => println(s"Unknown: '$path'")
+        case s => println(s"Unknown: '$s'")
+      }
+
+      Round(Hand(h1), Hand(h2), Hand(h3), Hand(h4), xs.reverse)
+    }
+  }
+
 }
