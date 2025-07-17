c1c47284d35f40990cf0ba944761e59b834e3b64
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2025-07-17 10:31:30 +0100

    1000

--- bench.cpp
+++ bench.cpp
@@ -15,7 +15,7 @@ void run_benchmark_full_game() {
         jass::c("6C,QS,8S,7S,AD,QD,JC,9C,8C"),
     };
 
-    int num_trials = 10;
+    int num_trials = 1000;
     std::vector<double> times;
     times.reserve(num_trials);
 
